51a39d7b86c9699680c563155567a503a9ca474f
MBS212 <163756197+MBS212@users.noreply.github.com>
2024-12-23 12:24:00 +0000

    Add files via upload

---
+++ Arduino Code/001-drivers-pico-mc.ino
@@ -0,0 +1,85 @@
+// main file of the project to:
+// - to switch on/off the custom cornering lights;
+// - to move servo with my GoPro camera attached to it;
+// based on the Mercedes S212 E300 Hybrid, 
+//   the sensor data aquired through the CAN bus frames
+
+#include "canbusreader.h"  // file to process CAN bus messages and get the values
+#include "servocontrol.h"  // file to control GoPro servo motor dep. on st. wheel angle
+#include "corneringlights.h"  // to control the cornering lights
+
+bool Debug = false;  // onboard neopix is OFF, set to true for debugging
+
+// trying to smooth servo movements with millis()
+unsigned long canTime = 0;
+unsigned long servoTime = 0;
+unsigned long pixelsTime = 0;
+
+void setup()  // rp2040 1st core setup
+{
+  SetupCan();     // defined in canfunctions.h
+  delay(1000);
+}
+
+void loop()  // rp2040 1st core loop
+{
+  // process CAN bus msgs and return the steering wheel angle (value betw. 3100 & 5100)
+  // this function is in canbusreader.h
+//  if( millis() - canTime >= 5 )
+//  {
+    ReadCAN();  
+//    canTime = millis();
+//  }
+}
+
+///////////////////////////////////////
+void setup1()  // rp2040 2nd core setup
+{
+  SetupServo();   // defined in servocontrol.h
+
+  SetupCorneringLights();
+
+  // Set the on-board Neopixel pin to output and high
+  //   in order to enable the NeoPixels. 
+  if( Debug )
+  {
+    pinMode(NEOPIXEL_POWER, OUTPUT);
+    digitalWrite(NEOPIXEL_POWER, HIGH);
+    pixels.begin();            // init. NeoPixel object
+    pixels.setBrightness(5);  // NeoPixel's brightness
+  }
+  delay(1000);
+}
+
+void loop1()  // rp2040 2nd core loop
+{
+  // if it's daylight,
+  // than move our GoPro's servo depending on
+  // the st. wheel angle, 
+  // else we don't need to control servo at night
+  if( !nightTime )
+  {
+    if( millis() - servoTime >= 10 )
+    {
+      MoveGopro();  // this function is in servocontrol.h
+      servoTime = millis();
+    }
+  }
+
+  // 29.04.2024:
+  //   Removed from here the condition if(nightTime)
+  //   because it would create a bug that it will not switch off the lights 
+  //   when nightTime changes to false (day time) while we are driving.
+  //   This condition is now added in the RunCorneringLights() function.
+  RunCorneringLights();
+
+  if( Debug )
+  {
+    if( millis() - pixelsTime >= 100 )
+    {
+      pixels.fill(currentPixColor); // set the pix color
+      pixels.show();                // light up the pix
+      pixelsTime = millis();
+    }
+  }
+}
---
+++ Arduino Code/corneringlights.h
@@ -0,0 +1,169 @@
+/*****************************************
+this file contains fuction that control
+which cornering light is on or off
+
+both ON if going backward
+left or right ON dep. on st. wheel angle and trun indicators
+*/
+#define OFFSET_DEGREES 150     // how many degrees from center, at which the corn. lights are to be sw. ON/OFF
+#define MAX_SPEED 18          // maximum speed in kmh the cornering lights to be enabled
+
+#include <Adafruit_NeoPixel.h>
+#include "hardware/pio.h"          // to control PIO
+#include "corneringlights.pio.h"   // RP2040 PIO assembled program to control the relays
+
+// use of pico's onboard neopixel for debugging & fault finding
+Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+   // putting our on-board neopixel to diff. colours for easier debugging
+int yellow = 0xbca600;  // cornering lights OFF
+int  green = 0x00aa00;  // left cornering light ON
+int    red = 0xaa0000;  // right cornering light ON
+int   blue = 0x0000aa;  // headlights ON, speed > 18 kmh
+int   grey = 0x333333;  // speed < 18, st. wheel within (-/+45 deg.)
+int purple = 0xaa00aa;  // corn. lights ON (both - vehicle going backward)
+int currentPixColor = 0x000000;
+
+//#define  leftRelay 1  // pin for the left corn. light relay
+//#define rightRelay 25  // pin for the right corn. light relay
+
+PIO pio = pio0;   // pio to use, we use only one and two of its state machines
+uint offset1;     // our pio program location in the memory
+uint smLeft;      // the state machine to use for the left relay
+uint smRight;     // the state machine to use for the right relay
+
+unsigned long corneringTime = 0;    // used with millis() to sw. corn. lights
+unsigned long switchingDelay = 100; // delay to switch OFF the corn. lights
+
+void SetupCorneringLights()
+{
+  offset1 = pio_add_program(pio, &corneringlights_program);
+  smLeft = pio_claim_unused_sm(pio, true);
+  corneringlights_program_init(pio, smLeft, offset1, 1);  // init our asm program to run on pin 1 (left)
+  smRight = pio_claim_unused_sm(pio, true);
+  corneringlights_program_init(pio, smRight, offset1, 25);  // init our asm program to run on pin 3 (right)
+
+//  pinMode(leftRelay, OUTPUT);     // pin 1
+//  pinMode(rightRelay, OUTPUT);    // pin 25
+
+// set initial values at pico start (ignition ON)
+      pio_sm_put_blocking(pio, smLeft, 0);  // switch on the left corn. light relay
+//      digitalWrite(leftRelay, LOW);       // switch on the left corn. light relay
+      pio_sm_put_blocking(pio, smRight, 0);  // switch on the right corn. light relay
+//      digitalWrite(rightRelay, LOW);      // switch on the right corn. light relay
+      currentPixColor = yellow;             // headlights ON, speed low & positive, but st. wheel within middle range
+}
+
+//////////////////////////////////
+// vars and function to check if the st. wheel is beyond the offset value
+uint16_t stCenter = 4096;              // the steering wheel center position
+uint16_t stOffset = OFFSET_DEGREES*2;  // the angle value from center, at which the corn. lights are to be sw. ON/OFF
+char     corneringLight = 'N';         // which corn. light to switch on, N - none, L - left, R - right
+
+void CheckStAngle()
+{
+  if( stAngleVal <= (stCenter - stOffset) )
+  {
+    corneringLight = 'L';      // for the left cornering light
+  }
+  else if( stAngleVal >= (stCenter + stOffset) )
+  {
+    corneringLight = 'R';      // for the right corn. light
+  }
+  else
+  {
+    corneringLight = 'N';      // none of the corn. light should be on
+  }
+}
+
+///////////////////////////////////////////////////
+// the function to switch on/off the corn. lights
+// based on the CAN bus data
+void SwitchCorneringLights()
+{
+  if( speedDirection > 4 )  // if we are going backward - keep both ON ?
+  {
+    pio_sm_put_blocking(pio, smLeft, 1);  // switch on the left corn. light relay
+//    digitalWrite(leftRelay, HIGH);     // switch on the left corn. light relay
+    pio_sm_put_blocking(pio, smRight, 1);  // switch on the right corn. light relay
+//    digitalWrite(rightRelay, HIGH);    // switch on the right corn. light relay
+    currentPixColor = purple;          // for debugging - purple means both should be ON
+    switchingDelay = 4000;    // set switch off delay after going back
+  }
+  else if( corneringLight == 'L' )
+  {
+    if( turnSignal == 'R' )
+    {
+      pio_sm_put_blocking(pio, smRight, 1);  // switch on the right corn. light relay
+      pio_sm_put_blocking(pio, smLeft, 0);  // switch on the left corn. light relay
+//      digitalWrite(rightRelay, HIGH);   // switch on the right corn. light relay
+//      digitalWrite(leftRelay, LOW);   // switch on the left corn. light relay
+      currentPixColor = red;              // right corn. light must now be on
+      switchingDelay = 3000;    // set switch off delay after turn signal
+    }
+    else
+    {
+      pio_sm_put_blocking(pio, smLeft, 1);  // switch on the left corn. light relay
+      pio_sm_put_blocking(pio, smRight, 0);  // switch on the right corn. light relay
+//      digitalWrite(leftRelay, HIGH);   // switch on the left corn. light relay
+//      digitalWrite(rightRelay, LOW);   // switch on the left corn. light relay
+      currentPixColor = green; // right corn. light must now be on
+      switchingDelay = 2000;    // set switch off delay after going forward
+    }
+  }
+  else if( corneringLight == 'R' )
+  {
+    if( turnSignal == 'L' )   
+    {
+      pio_sm_put_blocking(pio, smLeft, 1);  // switch on the left corn. light relay
+      pio_sm_put_blocking(pio, smRight, 0);  // switch on the left corn. light relay
+//      digitalWrite(leftRelay, HIGH);   // switch on the left corn. light relay
+//      digitalWrite(rightRelay, LOW);   // switch on the left corn. light relay
+      currentPixColor = green;                  // right corn. light must now be on
+      switchingDelay = 3000;    // set switch off delay after turn indicator
+    }
+    else
+    {
+      pio_sm_put_blocking(pio, smRight, 1);  // switch on the right corn. light relay
+      pio_sm_put_blocking(pio, smLeft, 0);  // switch on the left corn. light relay
+//      digitalWrite(rightRelay, HIGH);   // switch on the right corn. light relay
+//      digitalWrite(leftRelay, LOW);   // switch on the left corn. light relay
+      currentPixColor = red;                  // right corn. light must now be on
+      switchingDelay = 2000;    // set switch off delay after going forward
+    }
+  }
+  else
+  {
+    pio_sm_put_blocking(pio, smLeft, 0);  // switch on the left corn. light relay
+    pio_sm_put_blocking(pio, smRight, 0);  // switch on the right corn. light relay
+//    digitalWrite(leftRelay, LOW);   // switch on the left corn. light relay
+//    digitalWrite(rightRelay, LOW);  // switch on the right corn. light relay
+    currentPixColor = yellow;          // headlights ON, speed low & positive, but st. wheel within middle range
+    switchingDelay = 100;    // set switch off delay after going back
+  }
+}
+////////////////////////////////////
+// the function to schedule switching on/off the cornering lights
+void RunCorneringLights()
+{
+  // 29.04.24 - moved the nightTime condition from the loop1() to here
+  if( nightTime && currentSpeed < MAX_SPEED ) 
+  {
+    CheckStAngle();
+
+    if( millis() - corneringTime >= switchingDelay ) 
+    {
+      SwitchCorneringLights(); 
+      corneringTime = millis();
+    }
+  }
+  else
+  {
+    pio_sm_put_blocking(pio, smLeft, 0);  // switch on the left corn. light relay
+    pio_sm_put_blocking(pio, smRight, 0);  // switch on the right corn. light relay
+//    digitalWrite(leftRelay, LOW);   // switch on the left corn. light relay
+//    digitalWrite(rightRelay, LOW);  // switch on the right corn. light relay
+    currentPixColor = yellow;          // headlights/nightTime ON, speed low & positive, but st. wheel within middle range
+    switchingDelay = 100;              // set switch off delay to default
+  }
+}
